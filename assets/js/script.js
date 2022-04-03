b7b46905ef77e867a5bfa3bef19ea9d4a5492b2a
Lucas Pittam <98059989+lucaspittam@users.noreply.github.com>
2022-04-03 18:53:48 -0400

    preliminary js varibles

---
+++ assets/js/script.js
@@ -0,0 +1,60 @@
+var apiKey = "";
+
+
+var currentWeather = {
+}
+
+
+var forecast = [];
+var searchHistory = [];
+
+//querySelectors to reference in the sccript.
+var cityNameEl = document.querySelector("#name");
+var curDateEl = document.querySelector("#date");
+var curIconEl = document.querySelector("#icon");
+var curTempEl = document.querySelector("#temp");
+var curHumidityEl = document.querySelector("#humidity");
+var curWindEl = document.querySelector("#wind");
+var curUvEl = document.querySelector("#uv");
+var searchInputEl = document.querySelector("#search-city");
+var formEl = document.querySelector("#search-form");
+var historyEl = document.querySelector("#history");
+var clearBtnEl = document.querySelector("#clear-history");
+var uvAlertEl = document.querySelector("#uv-alert");
+var forecastEl = document.querySelector("#forecast-body");
+var resultsContEl = document.querySelector("#results-container");
+var forecastContEl = document.querySelector("#forecast-container");
+var curStatsEl = document.querySelector("#current-stats");
+
+
+var getWeather = function (){
+
+
+}
+
+
+
+var getForecast = function () {
+
+}
+
+
+var displayForecast = function () {
+
+
+}
+
+var displayWeather = function() {
+
+
+}
+
+
+var displayHistory = function() {
+
+}
+
+
+var loadHistory = function() {
+
+}
